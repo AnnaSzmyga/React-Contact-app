2e1ac80cbc9609f7c025fab90ef38c479fe3a12c
Anna Szmyga <anna.szmyga@o2.pl>
2019-03-19 09:52:31 +0100

    Change email link in contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -9,7 +9,8 @@ var Contact = React.createClass({
         <img className="contactImage" src="icon.jpg" />
         <p className="contactLabel">Imię: {this.props.item.firstName}</p>
         <p className="contactLabel">Nazwisko: {this.props.item.lastName}</p>
-        <a className="contactEmail" href={"mailto: " + this.props.item.email}> 
+        // <a className="contactEmail" href={"mailto: " + this.props.item.email}> 
+        <a className="contactEmail" href="`mailto: ${this.props.item.email}`">
           {this.props.item.email}</a>
       </div>
     )
